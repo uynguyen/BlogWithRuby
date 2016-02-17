5f4084c4b0c9e534adf030f27b9514b77c23caf7
Uy Nguyễn <leesan.itus@gmail.com>
2016-02-17 20:31:08 +0700

    Add searching feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,12 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    if params[:search]
+      @articles = Article.search(params[:search]).order("created_at DESC")
+    else
+      @articles = Article.all.order("created_at DESC")
+    end
+    
     @Markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+	def self.search(query)
+		where("title like ?", "%#{query}%")
+	end
 end
